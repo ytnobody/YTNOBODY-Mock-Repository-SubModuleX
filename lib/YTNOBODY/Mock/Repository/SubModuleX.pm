5fac9d90d6e9e522b2e8c53aeefe2a63c52fc06b
ytnobody <ytnobody@gmail.com>
2012-08-23 15:19:04 +0900

    initial commit

---
+++ lib/YTNOBODY/Mock/Repository/SubModuleX.pm
@@ -0,0 +1,32 @@
+package YTNOBODY::Mock::Repository::SubModuleX;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+YTNOBODY::Mock::Repository::SubModuleX -
+
+=head1 SYNOPSIS
+
+  use YTNOBODY::Mock::Repository::SubModuleX;
+
+=head1 DESCRIPTION
+
+YTNOBODY::Mock::Repository::SubModuleX is
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
